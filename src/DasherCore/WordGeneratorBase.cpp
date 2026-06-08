d9a9f9d3804842529ee4e3e9f3a77c05209fa078
will wade <willwade@gmail.com>
2026-06-08 11:28:57 +0100

    Fix GetSymbols build - use SymbolIsSpaceCharacter instead of GetSpaceSymbol

--- src/DasherCore/WordGeneratorBase.cpp
+++ src/DasherCore/WordGeneratorBase.cpp
@@ -14,16 +14,8 @@ void CWordGeneratorBase::GetSymbols(std::vector<symbol> &into) {
         std::stringstream line(s);
         CAlphabetMap::SymbolStream ss(line);
         for (symbol sym; (sym = ss.next(m_pAlphMap)) != -1;) {
-            if (!into.empty()) {
-                symbol lastSym = into.back();
-                if (sym == 0 && lastSym == m_pAlph->GetSpaceSymbol()) continue;
-                if (lastSym == 0) {
-                    into.pop_back();
-                    if (sym != 0 && sym != m_pAlph->GetSpaceSymbol())
-                        into.push_back(m_pAlph->GetSpaceSymbol());
-                }
-            }
-            into.push_back(sym);
+            if (!into.empty() && sym == 0 && m_pAlph->SymbolIsSpaceCharacter(into.back())) continue;
+            if (sym != 0 || !into.empty()) into.push_back(sym);
         }
         if (!into.empty()) {
             if (into.back() == 0) into.pop_back();
